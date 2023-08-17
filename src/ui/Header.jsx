d5709a3e679a0c845ecb5002adfea55a9142ae81
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-17 14:15:16 +0330

    Changing the project name

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,8 @@ export default function Header() {
 	return (
 		<header className="flex items-center justify-between border-b border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
 			<Link to="/" className="tracking-widest">
-				Fast React Pizza Co.
+				{/* eslint-disable-next-line react/no-unescaped-entities */}
+				Rapid 'Za Co.
 			</Link>
 			<SearchOrder />
 			<UserName />
