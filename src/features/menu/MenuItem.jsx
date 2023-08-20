43da7c706161049b55025becbfbbac6b4d0b7454
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-20 12:42:04 +0330

    Using DeleteItem component inside MenuItem + conditional rendering

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,11 +1,14 @@
 import {formatCurrency} from "../../utils/helpers.js";
 import Button from "../../ui/Button.jsx";
-import {useDispatch} from "react-redux";
-import {addItem} from "../cart/cartSlice.js";
+import {useDispatch, useSelector} from "react-redux";
+import {addItem, getCurrentQuantityById} from "../cart/cartSlice.js";
+import DeleteItem from "../cart/DeleteItem.jsx";
 
 function MenuItem({pizza}) {
 	const {id, name, unitPrice, ingredients, soldOut, imageUrl} = pizza;
 	const dispatch = useDispatch();
+	const currentQuantity = useSelector(getCurrentQuantityById(id));
+	const isInCart = currentQuantity > 0;
 
 	function handleAddToCart() {
 		const newItem = {
@@ -30,7 +33,8 @@ function MenuItem({pizza}) {
 					) : (
 						<p className="text-sm uppercase font-medium text-stone-500">Sold out</p>
 					)}
-					{!soldOut && (
+					{isInCart && <DeleteItem />}
+					{!soldOut && !isInCart && (
 						<Button type="small" onClick={handleAddToCart}>
 							Add to cart
 						</Button>
