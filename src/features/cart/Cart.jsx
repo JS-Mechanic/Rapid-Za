de8ebc798cda8f9758264f99386e5d0e29e36eca
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-20 12:08:35 +0330

    Removing fakeCart and importing relevant modules

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,31 +1,10 @@
 import {LinkButton} from "../../ui/LinkButton.jsx";
 import Button from "../../ui/Button.jsx";
 import CartItem from "./CartItem.jsx";
-import {useSelector} from "react-redux";
-
-const fakeCart = [
-	{
-		pizzaId: 12,
-		name: "Mediterranean",
-		quantity: 2,
-		unitPrice: 16,
-		totalPrice: 32,
-	},
-	{
-		pizzaId: 6,
-		name: "Vegetale",
-		quantity: 1,
-		unitPrice: 13,
-		totalPrice: 13,
-	},
-	{
-		pizzaId: 11,
-		name: "Spinach and Mushroom",
-		quantity: 1,
-		unitPrice: 15,
-		totalPrice: 15,
-	},
-];
+import {useDispatch, useSelector} from "react-redux";
+import {clearCart, getCart} from "./cartSlice.js";
+import {getUsername} from "../user/userSlice.js";
+import EmptyCart from "./EmptyCart.jsx";
 
 function Cart() {
 	const cart = fakeCart;
