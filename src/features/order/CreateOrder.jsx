89d7934582551c6c8bb03774077e9c8a1da0ca48
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-21 11:34:41 +0330

    Removing fake cart

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -12,30 +12,6 @@ import {useState} from "react";
 const isValidPhone = str =>
 	/^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
 
-const fakeCart = [
-	{
-		pizzaId: 12,
-		name: "Mediterranean",
-		quantity: 2,
-		unitPrice: 16,
-		totalPrice: 32,
-	},
-	{
-		pizzaId: 6,
-		name: "Vegetale",
-		quantity: 1,
-		unitPrice: 13,
-		totalPrice: 13,
-	},
-	{
-		pizzaId: 11,
-		name: "Spinach and Mushroom",
-		quantity: 1,
-		unitPrice: 15,
-		totalPrice: 15,
-	},
-];
-
 function CreateOrder() {
 	const username = useSelector(state => state.user.username);
 	const navigation = useNavigation();
