c552418ea68df33bd811e4eb438bf9aa77257ab1
Milad Sadeghi DM <miladsadeghidm@gmail.com>
2023-08-22 14:31:20 +0330

    Making the order a priority one after creating that

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,9 @@ import UserName from "../features/user/UserName.jsx";
 
 export default function Header() {
 	return (
-		<header className="flex items-center justify-between border-b border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
+		<header
+			className="flex items-center justify-between border-b border-stone-300
+		bg-yellow-400 px-4 py-3 uppercase sm:px-6">
 			<Link to="/" className="tracking-widest">
 				{/* eslint-disable-next-line react/no-unescaped-entities */}
 				Rapid 'Za Co.
